1a269b0a99571d3a4962bd02d963eeb2ea8ff983
ddisqq <ddisqq@users.noreply.github.com>
2026-03-15 00:12:41 -0400

    Relicense from BSD-3-Clause to Apache-2.0

--- cl-pathname-pure.asd
+++ cl-pathname-pure.asd
@@ -7,7 +7,7 @@
 (asdf:defsystem #:cl-pathname-pure
   :description "Pure Common Lisp cross-platform pathname handling library"
   :author "Parkian Company LLC"
-  :license "BSD-3-Clause"
+  :license "Apache-2.0"
   :version "0.1.0"
   :serial t
   :components ((:file "package")
--- test/test-pathname-pure.lisp
+++ test/test-pathname-pure.lisp
@@ -1,10 +1,10 @@
 ;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;; SPDX-License-Identifier: BSD-3-Clause
+;; SPDX-License-Identifier: Apache-2.0
 
 ;;;; test-pathname-pure.lisp - Unit tests for pathname-pure
 ;;;;
 ;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
-;;;; SPDX-License-Identifier: BSD-3-Clause
+;;;; SPDX-License-Identifier: Apache-2.0
 
 (defpackage #:cl-pathname-pure.test
   (:use #:cl)
